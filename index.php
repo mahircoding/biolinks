4ca7b7e49ed0aea50d78d04c687dc628f204519a
Your Name <you@example.com>
2025-10-04 01:46:55 +0700

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Enabling debug mode is only for debugging / development purposes. */
-define('DEBUG', false);
+define('DEBUG', true);
 
 /* Enabling mysql debug mode is only for debugging / development purposes. */
 define('MYSQL_DEBUG', false);
