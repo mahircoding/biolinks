fefda446943fc89d3cfe2f9dcd44d6ef13ae5c2a
Your Name <you@example.com>
2025-10-09 22:12:26 +0700

    update

--- app/controllers/UserProducts.php
+++ app/controllers/UserProducts.php
@@ -178,11 +178,11 @@ class UserProducts extends Controller {
             }
             /* If Tripay configured for this user and addon enabled, create transaction and redirect to payment page */
             else {
-                $reference = 'KBIO-123';
+                $reference = 'KBIO-' . $order->order_id;
 
                 $payload = [
                     'method'        => $payment_method ?: 'QRIS',
-                    'merchant_ref'  => $reference,
+                    'merchant_ref'  => $reference ,
                     'amount'        => $amount_cents,
                     'customer_name' => $name,
                     'customer_email'=> $email,
