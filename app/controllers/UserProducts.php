903900dbe5ed37334fa4f26fe3c504228067abf1
Your Name <you@example.com>
2025-10-09 22:58:39 +0700

    update

--- app/controllers/UserProducts.php
+++ app/controllers/UserProducts.php
@@ -178,7 +178,8 @@ class UserProducts extends Controller {
             }
             /* If Tripay configured for this user and addon enabled, create transaction and redirect to payment page */
             else {
-                $order = Database::get(['order_id']);
+                $order = Database::get(['order_id', 'download_token', 'created_at', 'amount_cents'], DigitalOrderModel::$table, ['download_token' => $token]);
+                    
                 $reference = 'KBIO-' . $order->order_id;
 
                 $payload = [
