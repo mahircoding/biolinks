8d697be1c2207af0614b4380a7551c2420781885
Your Name <you@example.com>
2025-10-09 08:07:19 +0700

    update

--- themes/altum/views/admin/partials/admin_sidebar.php
+++ themes/altum/views/admin/partials/admin_sidebar.php
@@ -273,9 +273,6 @@
                 </div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
-                <a class="dropdown-item" href="<?= url('digital-product') ?>"><i class="fa fa-fw fa-sm fa-box-open mr-1"></i> Digital Product</a>
-                <a class="dropdown-item" href="<?= url('digital-order') ?>"><i class="fa fa-fw fa-sm fa-receipt mr-1"></i> Order</a>
-                <a class="dropdown-item" href="<?= url('tripay-settings') ?>"><i class="fa fa-fw fa-sm fa-credit-card mr-1"></i> Tripay Settings</a>
                 <a class="dropdown-item" href="<?= url('account') ?>"><i class="fa fa-fw fa-sm fa-sm fa-wrench mr-1"></i> <?= $this->language->account->menu ?></a>
                 <a class="dropdown-item" href="<?= url('logout') ?>"><i class="fa fa-fw fa-sm fa-sm fa-sign-out-alt mr-1"></i> <?= $this->language->global->menu->logout ?></a>
             </div>
