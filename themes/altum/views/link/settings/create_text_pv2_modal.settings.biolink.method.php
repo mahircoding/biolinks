dfa6f2a6d697bbfb2ce5e54abeb7409fe6fb4409
Your Name <you@example.com>
2026-02-21 15:44:02 +0700

    fix HTML PV2

--- themes/altum/views/link/settings/create_text_pv2_modal.settings.biolink.method.php
+++ themes/altum/views/link/settings/create_text_pv2_modal.settings.biolink.method.php
@@ -3,7 +3,6 @@
 <div class="modal fade" id="create_biolink_text_pv2" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
-
             <div class="modal-header">
                 <h5 class="modal-title"><?= $this->language->create_biolink_text_pv2_modal->header ?></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
