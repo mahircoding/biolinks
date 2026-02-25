750c30fb02d8be1c5f79bacb8e4cccb172d556fe
Your Name <you@example.com>
2026-02-25 10:55:53 +0700

    html pv2 full page

--- themes/altum/views/link-path/partials/biolink_link_text_pv2.php
+++ themes/altum/views/link-path/partials/biolink_link_text_pv2.php
@@ -1,6 +1,3 @@
 <?php defined('ALTUMCODE') || die() ?>
 
-<div>
-    <h2><?= $data->link->settings->title ?></h2>
-    <p><?= $data->link->settings->description ?></p>
-</div>
+<?= $data->link->settings->description ?>
